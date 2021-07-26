50025faacc2db4bbc78b67bbd0005924d240080b
Swati Bhat <swati.bhat@thoughtworks.com>
2021-07-26 17:05:13 +0530

    updated the code with oop concepts

--- src/main/java/FourBalls.java
+++ src/main/java/FourBalls.java
@@ -1,8 +1,17 @@
 import processing.core.PApplet;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FourBalls extends PApplet {
 
-    int speed = 1;
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 640;
+    public static final int DIAMETER = 10;
+    public static final int TOTAL_NUMBER_OF_BALLS = 4;
+    private int speed = 1;
+    private List<Integer> speedRateOfBalls = Arrays.asList(1, 2, 3, 4);
+    private List<Float> heightRatioFromTop = Arrays.asList(0.2F, 0.4F, 0.6F, 0.8F);
 
     public static void main(String[] args) {
         PApplet.main("FourBalls", args);
@@ -11,15 +20,26 @@ public class FourBalls extends PApplet {
     @Override
     public void settings() {
         super.settings();
-        size(480, 640);
+        size(WIDTH, HEIGHT);
     }
 
     @Override
     public void draw() {
-        ellipse(speed, (float) (640 * 0.2), 10, 10);
-        ellipse(speed * 2, (float) (640 * 0.4), 10, 10);
-        ellipse(speed * 3, (float) (640 * 0.6), 10, 10);
-        ellipse(speed * 4, (float) (640 * 0.8), 10, 10);
+        for (int index = 0; index < TOTAL_NUMBER_OF_BALLS; index++) {
+            drawCircle(getXCoordinate(speed, speedRateOfBalls.get(index)), getYCoordinate(heightRatioFromTop.get(index)));
+        }
         speed++;
+    }
+
+    private void drawCircle(float xCoordinate, float yCoordinate) {
+        ellipse(xCoordinate, yCoordinate, DIAMETER, DIAMETER);
+    }
+
+    private float getYCoordinate(float heightRatioFromTop) {
+        return HEIGHT * heightRatioFromTop;
+    }
+
+    private int getXCoordinate(int speed, int speedRate) {
+        return speed * speedRate;
     }
 }

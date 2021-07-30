a0fad1fe1fac51796601516b523e8dfee0923ece
Swati Bhat <swati.bhat@thoughtworks.com>
2021-07-30 12:18:15 +0530

    Refactored the CoordinateSysytem model class

--- src/main/java/model/CoordinateSystem.java
+++ src/main/java/model/CoordinateSystem.java
@@ -5,21 +5,25 @@ public class CoordinateSystem {
     private float xCoordinate;
     private float yCoordinate;
 
-    private static int ratio = 0;
+    private static int itemNumber = 0;
 
     public CoordinateSystem(int x, int height, int items) {
-        ++ratio;
+        ++itemNumber;
         this.xCoordinate = setXCoordinate(x, items);
         this.yCoordinate = setYCoordinate(height, items);
     }
 
     private float setXCoordinate(int x, int items) {
-        return x * getRatio(items);
+        return x * getItemNumber(items);
+    }
+
+    private float getItemNumber(int items) {
+        return itemNumber % items != 0  ? itemNumber % items : items;
     }
 
     private float setYCoordinate(int height, int items) {
         float heightRatio = 0.2F;
-        return height * getRatio(items) * heightRatio;
+        return height * getItemNumber(items) * heightRatio;
     }
 
     public float getXCoordinate() {
@@ -30,8 +34,5 @@ public class CoordinateSystem {
         return yCoordinate;
     }
 
-    private float getRatio(int items) {
-        return ratio % items != 0 ? ratio % items : items;
-    }
 
 }
